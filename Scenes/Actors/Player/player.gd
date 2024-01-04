e29dc3d82c4c278d5a1329d77cdd604bb7f355fb
Giulio Sá <giuliocaetano@yahoo.com.br>
2024-01-04 16:04:16 -0300

    Setting the basic animation

--- Scenes/Actors/Player/player.gd
+++ Scenes/Actors/Player/player.gd
@@ -1,6 +1,31 @@
 extends CharacterBody2D
 
+@onready var animation_tree: AnimationTree = %AnimationTree
+
+var direction: Vector2 = Vector2.ZERO
+
+func _ready():
+	animation_tree.active = true
+	
+func _process(delta):
+	update_animation_parameters()
+
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right","move_up","move_down")
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	velocity = direction * 200
 	move_and_slide()
+	
+
+func update_animation_parameters():
+	if velocity == Vector2.ZERO:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_walking"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_walking"] = true
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/Idle/blend_position"] = direction
+		animation_tree["parameters/Walk/blend_position"] = direction
+	
+
+
