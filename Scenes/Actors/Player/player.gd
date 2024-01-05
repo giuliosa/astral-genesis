5897cf6f37517cf32529799cdd65c35ebe3eb1c8
Giulio Sá <giuliocaetano@yahoo.com.br>
2024-01-05 10:22:29 -0300

    Stairs 01 scene

--- Scenes/Actors/Player/player.gd
+++ Scenes/Actors/Player/player.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	velocity = direction * 200
+	velocity = direction * 100
 	move_and_slide()
 	
 
